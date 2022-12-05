60d0ab82081ab45364ca468135236448181f88e6
Gallahar <david.kushnir.d@gmail.com>
2022-12-05 12:35:38 +0300

    theme/dark/light saved after refresh

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,4 +1,4 @@
-import styles from "./index.module.scss";
+import "./index.scss";
 import user from "../../assets/user.svg";
 import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/GiteOutlined";
@@ -8,31 +8,52 @@ import SearchIcon from "@mui/icons-material/SearchOutlined";
 import UserIcon from "@mui/icons-material/AccountBoxOutlined";
 import MessageIcon from "@mui/icons-material/EmailOutlined";
 import NotificationIcon from "@mui/icons-material/CircleNotificationsOutlined";
+import React from "react";
+import { useAppDispatch } from "../../store";
+import { setTheme } from "../../store/theme/slice";
 
-export const NavBar = () => {
+type TNavBarProps = {
+  theme: boolean;
+};
+
+export const NavBar: React.FC<TNavBarProps> = React.memo(({ theme }) => {
+  const firstRender = React.useRef(false);
+  const dispatch = useAppDispatch();
+  React.useEffect(() => {
+    if (firstRender.current) {
+      const dataToLocal = JSON.stringify(theme);
+      localStorage.setItem("theme", dataToLocal);
+    }
+    firstRender.current = true;
+  }, [theme]);
+
+  const onClickTheme = () => {
+    dispatch(setTheme(!theme));
+  };
+
   return (
-    <div className={styles.navbar}>
-      <div className={styles.left}>
+    <div className="navbar">
+      <div className={"left"}>
         <Link to="/" style={{ textDecoration: "none" }}>
           <span>HomePage</span>
         </Link>
         <HomeIcon />
-        <ThemeIcon />
+        <ThemeIcon className={"themeIcon"} onClick={onClickTheme} />
         <AppIcon />
-        <div className={styles.search}>
+        <div className={"search"}>
           <SearchIcon />
           <input type="text" placeholder="search someone.." />
         </div>
       </div>
-      <div className={styles.right}>
+      <div className={"right"}>
         <UserIcon />
         <MessageIcon />
         <NotificationIcon />
-        <div className={styles.user}>
+        <div className={"user"}>
           <img src={user} alt="user" />
           <span>James Brune</span>
         </div>
       </div>
     </div>
   );
-};
+});
