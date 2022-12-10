9b90fc367d9f1345be57f41f0c0535aedfe2790d
Gallahar <david.kushnir.d@gmail.com>
2022-12-10 23:14:15 +0300

    Profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,72 @@
-import styles from 'index.module.scss'
+import "./index.scss";
+import { useSelector } from "react-redux";
+import { selectAuth } from "../../store/auth/selectors";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+const Profile = () => {
+  const { user } = useSelector(selectAuth);
+  return (
+    <div className={"profile"}>
+      <div className={"imgContainer"}>
+        <img
+          src={user ? user.profilePicture : ""}
+          alt={"profilePicture"}
+          className={"background"}
+        />
+        <img
+          src={user ? user.imgUrl : ""}
+          alt={"userPicture"}
+          className={"userImg"}
+        />
+      </div>
+      <div className={"profileContainer"}>
+        <div className={"socialsContainer"}>
+          <div className={"left"}>
+            <a href={"https://vk.com"} target={"_blank"}>
+              <FacebookTwoToneIcon fontSize={"large"} />
+            </a>
+            <a>
+              <LinkedInIcon fontSize={"large"} />
+            </a>
+            <a>
+              <InstagramIcon fontSize={"large"} />
+            </a>
+            <a>
+              <PinterestIcon fontSize={"large"} />
+            </a>
+            <a>
+              <TwitterIcon fontSize={"large"} />
+            </a>
+          </div>
+          <div className={"center"}>
+            <span>Jane Doe</span>
+            <div className={"infoContainer"}>
+              <div className={"item"}>
+                <PlaceIcon fontSize={"small"} />
+                <span>IL</span>
+              </div>
+              <div className={"item"}>
+                <LanguageIcon fontSize={"small"} />
+                <span>gala</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className={"right"}>
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-const Profile = () =>{
-    return (
-        <div className={styles.profile}></div>
-    )
-}
-
-export default Profile
+export default Profile;
