aef1ee6e6e407b9df334b8b14bd92dfbb199e4e6
Gallahar <david.kushnir.d@gmail.com>
2022-12-23 11:00:49 +0300

    auth:login/register functions

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,47 +1,66 @@
-import {db} from "../connect.js";
-import bcrypt from 'bcryptjs'
+import { db } from "../connect.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const register = (req,res)=>{
-const query = "SELECT * FROM users WHERE username = ?"
-    db.query(query,[req.body.username],(error,data)=>{
-        if(error){
-            return res.status(500).json(error)
-        }
-        if(data.length){
-            return res.status(409).json("user already exist")
-        }
-        const salt = bcrypt.genSaltSync(10)
-        const hashPassword = bcrypt.hashSync(req.body.password, salt)
+export const register = (req, res) => {
+  const query = "SELECT * FROM users WHERE username = ?";
+  db.query(query, [req.body.username], (error, data) => {
+    if (error) return res.status(500).json(error);
+    if (data.length) return res.status(409).json("user already exist");
 
-        const query = "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)"
-        const dataArr = [req.body.username,req.body.email,hashPassword,req.body.name]
-        db.query(query,[dataArr],(error,data)=>{
-            if(error){
-                return res.status(500).json(error)
-            }
-            return res.status(201).json("User has been created!")
-        })
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const query =
+      "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
+    const dataArr = [
+      req.body.username,
+      req.body.email,
+      hashPassword,
+      req.body.name,
+    ];
+    db.query(query, [dataArr], (error, data) => {
+      if (error) {
+        return res.status(500).json(error);
+      }
+      return res.status(201).json("User has been created!");
+    });
+  });
+};
+
+export const login = (req, res) => {
+  const query = "SELECT * FROM users WHERE username = ?";
+  db.query(query, [req.body.username], (error, data) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    if (data.length === 0) {
+      return res.status(404).json("user not found");
+    }
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+    if (!checkPassword) {
+      return res.status(400).json("wrong username/password");
+    }
+    const jwtToken = jwt.sign({ id: data[0].id }, "key");
+    const { password, ...rest } = data[0];
+    res
+      .cookie("accessToken", jwtToken, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+  });
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
     })
-
-}
-
-
-export const login = (req,res)=>{
-const query = "SELECT * FROM users WHERE username = ?"
-    db.query(query,[req.body.username],(error,data)=>{
-        if(error){
-            return res.status(500).json(error)
-        }
-        if(data.length===0){
-            return res.status(404).json("user not found")
-        }
-        const checkPassword = bcrypt.compareSync(req.body.password, data[0].password)
-        if(!checkPassword){
-            return res.status(400).json("wrong username/password")
-        }
-    })
-}
-
-export const logout = (req,res)=>{
-
-}
+    .status(200)
+    .json("user successfully logout");
+};
--- server/index.js
+++ server/index.js
@@ -1,24 +1,33 @@
-import express from 'express'
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import express from "express";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 
-const app = express()
+const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 
-app.use(express.json())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(cookieParser());
 
+app.use("/server/auth", authRoutes);
+app.use("/server/users", userRoutes);
+app.use("/server/posts", postRoutes);
+app.use("/server/likes", likeRoutes);
+app.use("/server/comments", commentRoutes);
 
-
-app.use("/api/auth",authRoutes)
-app.use("/api/users",userRoutes)
-app.use("/api/posts",postRoutes)
-app.use("/api/likes",likeRoutes)
-app.use("/api/comments",commentRoutes)
-
-
-app.listen(8900,()=>{
-    console.log('Successfully connected!')
-})
+app.listen(8900, () => {
+  console.log("Successfully connected!");
+});
