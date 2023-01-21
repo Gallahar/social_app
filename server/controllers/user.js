7f02db68162fac6748b14f49e54f0b55457cb53f
Gallahar <david.kushnir.d@gmail.com>
2023-01-21 17:35:57 +0300

    logic for getting users by name, getting all users

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,77 @@
-export const getUser = (req,res)=>{
+import jwt from "jsonwebtoken";
+import { db } from "../connect.js";
 
-}
+export const getUser = (req, res) => {
+  const jwtToken = req.cookies.accessToken;
+  if (!jwtToken) return res.status(401).json("Authorization is required");
+  jwt.verify(jwtToken, "key", (error, data) => {
+    if (error)
+      return res.status(403).json("Authorization is not valid/expired");
+    const query = `SELECT * from users WHERE id = ? `;
+    db.query(query, [req.params.userId], (error, data) => {
+      if (error) return res.status(500).json(error);
+      if (data.length) {
+        const { password, ...rest } = data[0];
+        return res.status(200).json(rest);
+      } else {
+        return res.status(404).json("User not found");
+      }
+    });
+  });
+};
+
+export const patchUser = (req, res) => {
+  const jwtToken = req.cookies.accessToken;
+  if (!jwtToken) return res.status(401).json("Authorization is required");
+  jwt.verify(jwtToken, "key", (error, data) => {
+    if (error)
+      return res.status(403).json("Authorization is not valid/expired");
+    const query =
+      "UPDATE users SET `name`=?,`backgroundPic`=?,`profilePic`=?,`location`=?,`website`=? WHERE id=? ";
+    db.query(
+      query,
+      [
+        req.body.name,
+        req.body.backgroundPic,
+        req.body.profilePic,
+        req.body.location,
+        req.body.website,
+        data.id,
+      ],
+      (error) => {
+        if (error) return res.status(500).json(error);
+        return res.status(200).json("user parameters was updated!");
+      }
+    );
+  });
+};
+
+export const getSearchUser = (req, res) => {
+  const jwtToken = req.cookies.accessToken;
+  if (!jwtToken) return res.status(401).json("Authorization is required");
+  jwt.verify(jwtToken, "key", (error, data) => {
+    if (error)
+      return res.status(403).json("Authorization is not valid/expired");
+    const query = `SELECT * from users WHERE id != ? `;
+    db.query(query, [req.params.userId], (error, data) => {
+      const [...rest] = data;
+      if (error) return res.status(500).json(error);
+      return res.status(200).json(rest.filter((obj) => delete obj.password));
+    });
+  });
+};
+
+export const getUserByName = (req, res) => {
+  const jwtToken = req.cookies.accessToken;
+  if (!jwtToken) return res.status(401).json("Authorization is required");
+  jwt.verify(jwtToken, "key", (error, data) => {
+    if (error)
+      return res.status(403).json("Authorization is not valid/expired");
+    const query = `SELECT * from users WHERE name LIKE '%' ? '%' `;
+    db.query(query, [req.params.name], (error, data) => {
+      if (!data.length) return res.status(404).json("User not found");
+      if (error) return res.status(500).json(error);
+      return res.status(200).json(data.map(({ password, ...data }) => data));
+    });
+  });
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,14 @@
 import express from "express";
-import {getUser} from "../controllers/user.js";
+import {
+  getSearchUser,
+  getUser,
+  getUserByName,
+  patchUser,
+} from "../controllers/user.js";
 
-const router = express.Router()
-router.get("/find/userId",getUser)
-
-export default router
+const router = express.Router();
+router.get("/find/:userId", getUser);
+router.put("/", patchUser);
+router.get("/:userId", getSearchUser);
+router.get("/name/:name", getUserByName);
+export default router;
